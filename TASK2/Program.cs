1e5268dd64629371a52e93039ca73db79212076f
Elena <panovaelena1@yandex.ru>
2023-02-11 12:07:18 +0300

    Added all the tasks

---
+++ TASK2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента 
+//или же указание, что такого элемента нет.
+
+//Например, задан массив:
+
+//1 4 7 2
+
+//5 9 2 3
+
+//8 4 2 4
+
+//17 -> такого числа в массиве нет
+
+
+var arr = Generate2DArray(3, 4);
+Print2DArray (arr);
+
+Console.WriteLine("Введите индекс строки");
+int FirstNumb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца");
+int SecondNumb = Convert.ToInt32(Console.ReadLine());
+{
+    if (FirstNumb < arr.GetLength(0) && SecondNumb < arr.GetLength(1)) Console.WriteLine(arr[FirstNumb, SecondNumb]);
+    else Console.WriteLine ("такого числа в массиве нет");
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ TASK3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+var arr = Generate2DArray(3, 4);
+Print2DArray (arr);
+System.Console.WriteLine();
+
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];   
+        }
+         sum = sum / arr.GetLength(0);
+         System.Console.Write(sum);
+         System.Console.Write(" ");
+    }
+    
+    
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for(int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 5);
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
